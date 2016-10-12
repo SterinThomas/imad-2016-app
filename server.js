6821ef15efedcc5e9dc3b19d1c7caa3adf49844b
SterinThomas <sterinthomas97@gmail.com>
2016-10-12 08:56:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/article-one',function (req,res) {
     res.send('we be considered later');
 });
+app.get('/article-two',function (req,res) {
+    res.send('we be considered later1');
+});
+app.get('/article-three',function (req,res) {
+    res.send('we be considered later2');
+});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
