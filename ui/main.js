d5d759e7e7b77ac48ae050a4bfaaf9158086e752
SterinThomas <sterinthomas97@gmail.com>
2016-10-12 17:40:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "STEFY THOMAS";
+var img = document.getElementById("madi");
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
